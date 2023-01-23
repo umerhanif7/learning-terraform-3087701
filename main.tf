2717560e7d7c3f5cb3b29e9f637234c91c937959
UmerH <UmerH@N-UMERH>
2023-01-23 18:40:59 +0500

    Commit changes

--- main.tf
+++ main.tf
@@ -58,7 +58,7 @@ module "blog_alb" {
   load_balancer_type = "application"
 
   vpc_id             = module.blog_vpc.vpc_id
-  subnets            = module.blog_vpc.public_subnetss
+  subnets            = module.blog_vpc.public_subnets
   security_groups    = [module.blog_sg.security_group_id]
 
   target_groups = [
@@ -67,11 +67,6 @@ module "blog_alb" {
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
-      target = {
-        my_target = {
-          target_id = aws_instace.blog.id
-        }
-      }
     }
   ]
 
